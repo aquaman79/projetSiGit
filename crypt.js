4c5edbb9de46e8ea90271153a1d3d8fe4ead1e49
aquaman79 <qquamar1@gmail.com>
2023-01-24 09:49:09 +0100

    fix: nom de le fichier est de la function

---
+++ crypt.js
@@ -0,0 +1,16 @@
+function encryptText(name, key) {
+    key = key % 26; // pour éviter les clés trop grandes
+    var encryptedName = "";
+    for (var i = 0; i < name.length; i++) {
+        var charCode = name.charCodeAt(i);
+        if (charCode >= 65 && charCode <= 90) {
+            charCode = (charCode - 65 + key) % 26 + 65;
+        } else if (charCode >= 97 && charCode <= 122) {
+            charCode = (charCode - 97 + key) % 26 + 97;
+        }
+        encryptedName += String.fromCharCode(charCode);
+    }
+    return encryptedName;
+}
+
+
